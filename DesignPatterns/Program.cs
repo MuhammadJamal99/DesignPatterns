25a8652a642fa00b042a699f780822a402f08d4d
MuhammadJamal99 <muhammadjammi18@gmail.com>
2023-07-08 16:52:41 +0300

    Task #1 Show The Problem that Singleton Design Pattern Will Solve

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,13 +1,47 @@
-﻿namespace DesignPatterns;
+﻿using Singleton.Start;
+
+namespace DesignPatterns;
 
 internal class Program
 {
-    public void SingletonDesignPattern() 
+    #region Creational Design Patterns
+    #region 1- Singleton Pattern
+
+    private static MemoryLogger Logger;
+
+    private static void AssignVoucher(string email, string voucher) 
     {
+        Logger = new MemoryLogger();
+
+        Logger.LogInfo($"Voucher '{voucher}' Assigned");
+
+        Logger.LogError($"Unable to Send Email '{email}'");
 
     }
-    static void Main(string[] args) 
+
+    private static void UseVoucher(string voucher)
     {
-        Console.WriteLine("Hello, World!");
+        Logger = new MemoryLogger();
+
+        Logger.LogWarning($"3 Attempts Made To Validate The Voucher {voucher}");
+
+        Logger.LogInfo($"{voucher} Is Used!");
+    }
+    private static void SingletonDesignPattern() 
+    {
+        AssignVoucher("muhammadjammi18@gmail.com", "ABC123");
+        
+        UseVoucher("ABC123");
+        
+        Logger.ShowLog();
+    }
+
+    #endregion
+        
+    #endregion
+    private static void Main(string[] args) 
+    {
+        SingletonDesignPattern();
+        Console.ReadKey();
     }
 }
